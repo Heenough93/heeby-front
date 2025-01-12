c73999e62dbf29f58b7ae152eb196c5f43e03ce5
wheelbottle <park.hee.byeong@google.com>
2025-01-12 17:50:28 +0900

    :art: apply - vite, typescript, react

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function App() {
+  const heeby = `
+              __              __
+             / /_  ___  ___  / /_  __  __
+            / __ \\/ _ \\/ _ \\/ __ \\/ / / /
+           / / / /  __/  __/ /_/ / /_/ /
+          /_/ /_/\\___/\\___/_.___/\\__, /
+                                /____/
+  `
+
+  return (
+    <>
+      <pre style={{"display": "flex", "justifyContent": "center"}}>
+        {heeby}
+      </pre>
+    </>
+  )
+}
+
+export default App
